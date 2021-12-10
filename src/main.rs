e19184c7c097900ba3bb74a667b0eb830f474b1c
Joshua Warner <joshuawarner32@gmail.com>
2021-12-09 20:25:50 -0800

    visualizing traces

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ use crossterm::terminal::ClearType;
 use crossterm::{cursor, event, execute, queue, style, terminal};
 use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::Hash;
+use std::hash::Hasher;
 use std::io::{stdout, Write};
+use std::path::Path;
 use std::path::PathBuf;
+use std::rc::Rc;
 use std::time::{Duration, Instant};
 use std::{cmp, env, fs, io};
 
@@ -17,8 +22,6 @@ struct CleanUp;
 
 impl Drop for CleanUp {
     fn drop(&mut self) {
-        terminal::disable_raw_mode().expect("Unable to disable raw mode");
-        Output::clear_screen().expect("error");
     }
 }
 
@@ -54,29 +57,14 @@ impl StatusMessage {
 }
 
 struct Row {
+    offset: usize,
     row_content: String,
-    render: String,
+    render: RenderBuf,
+    column_offsets: Vec<usize>,
 }
 
-impl Row {
-    fn new(row_content: String, render: String) -> Self {
-        Self {
-            row_content,
-            render,
-        }
-    }
-
-    fn insert_char(&mut self, at: usize, ch: char) {
-        self.row_content.insert(at, ch);
-        EditorRows::render_row(self)
-    }
-
-    fn delete_char(&mut self, at: usize) {
-        self.row_content.remove(at);
-        EditorRows::render_row(self)
-    }
-}
 #[derive(serde::Deserialize)]
+#[derive(Eq, PartialEq, Hash)]
 struct EncodedFrame {
     addr: Option<u64>,
     name: Option<std::string::String>,
@@ -88,8 +76,8 @@ struct EncodedFrame {
 #[derive(serde::Deserialize)]
 struct EncodedFile {
     addrs: HashMap<u64, Vec<EncodedFrame>>,
-    frames: Vec<(u64, usize)>,
-    ranges: Vec<(usize, usize)>,
+    frames: Vec<(u64, u32)>,
+    ranges: Vec<(u32, usize)>,
     text: std::string::String,
 }
 
@@ -98,29 +86,122 @@ struct Buffer {
     cursor: CursorController,
 }
 
+#[derive(Eq, PartialEq, Hash)]
+struct TracedAddr(Vec<EncodedFrame>);
+
+#[derive(Eq, PartialEq, Hash)]
+struct TracedFrame {
+    height: usize,
+    addr: Rc<TracedAddr>,
+    parent: Option<Rc<TracedFrame>>,
+}
+
+struct TracedBuffer {
+    addrs: HashMap<u64, Rc<TracedAddr>>,
+    frames: Vec<Rc<TracedFrame>>,
+    by_offset: Vec<Rc<TracedFrame>>,
+    heights: Vec<usize>,
+    min_height: usize,
+    max_height: usize,
+    stack_hashes: Vec<u64>,
+    text: String,
+}
+
+impl TracedBuffer {
+    fn from_file(file: &Path) -> Self {
+        let data = fs::read_to_string(&file).expect("Unable to read file");
+        let raw: EncodedFile = serde_json::from_str(&data).unwrap();
+
+        let addrs = raw.addrs.into_iter()
+            .map(|(addr, frames)| {
+                (addr, Rc::new(TracedAddr(frames)))
+            })
+            .collect::<HashMap<_, _>>();
+
+        let mut frames = Vec::<Rc<TracedFrame>>::new();
+        for (addr, parent) in raw.frames {
+            let (parent, height) = if parent == u32::MAX {
+                (None, 1)
+            } else {
+                let parent = &frames[parent as usize];
+                (Some(parent.clone()), parent.height + 1)
+            };
+            let frame = Rc::new(TracedFrame {
+                addr: addrs[&addr].clone(),
+                parent,
+                height,
+            });
+            frames.push(frame);
+        }
+
+        let mut by_offset = Vec::with_capacity(raw.text.len());
+
+        for (frame, count) in raw.ranges {
+            let frame = &frames[frame as usize];
+            for _ in 0..count {
+                by_offset.push(frame.clone());
+            }
+        }
+
+        let heights = by_offset.iter().map(|f| f.height).collect::<Vec<_>>();
+
+        let min_height = heights.iter().min().copied().unwrap_or(0);
+        let max_height = heights.iter().max().copied().unwrap_or(0);
+
+        let stack_hashes = by_offset.iter().map(|f| {
+            let mut hasher = DefaultHasher::new();
+            f.hash(&mut hasher);
+            hasher.finish()
+        }).collect::<Vec<_>>();
+
+        Self {
+            addrs,
+            frames,
+            heights,
+            min_height,
+            max_height,
+            stack_hashes,
+            by_offset,
+            text: raw.text,
+        }
+    }
+
+    fn contents(&self) -> &str {
+        &self.text
+    }
+}
+
 struct EditorRows {
     external_files: HashMap<PathBuf, Buffer>,
     row_contents: Vec<Row>,
     filename: Option<PathBuf>,
-    raw: EncodedFile,
+    traced: TracedBuffer,
 }
 
 impl EditorRows {
     fn from_file(file: PathBuf) -> Self {
-        let data = fs::read_to_string(&file).expect("Unable to read file");
-        let raw: EncodedFile = serde_json::from_str(&data).unwrap();
-        let file_contents = raw.text.clone();
+        let traced = TracedBuffer::from_file(&file);
         let mut row_contents = Vec::new();
-        file_contents.lines().enumerate().for_each(|(i, line)| {
-            let mut row = Row::new(line.into(), String::new());
-            Self::render_row(&mut row);
+        let text = traced.contents();
+        let newlines = text.match_indices('\n').map(|(i, _)| i);
+        let mut i = 0;
+        for n in newlines {
+            let line = &text[i..n];
+            let mut row = Row {
+                offset: i,
+                row_content: line.into(),
+                render: RenderBuf::new(),
+                column_offsets: Vec::new(),
+            };
+            Self::render_row(&mut row, &traced);
             row_contents.push(row);
-        });
+            i = n + 1;
+        }
         Self {
             external_files: HashMap::new(),
             filename: Some(file),
             row_contents,
-            raw,
+            traced,
         }
     }
 
@@ -136,14 +217,28 @@ impl EditorRows {
         &self.row_contents[at]
     }
 
-    fn render_row(row: &mut Row) {
+    fn render_row(row: &mut Row, traced: &TracedBuffer) {
         let mut index = 0;
         let capacity = row
             .row_content
             .chars()
             .fold(0, |acc, next| acc + if next == '\t' { TAB_STOP } else { 1 });
-        row.render = String::with_capacity(capacity);
-        row.row_content.chars().for_each(|c| {
+        row.render.content.clear();
+        row.column_offsets.clear();
+        row.row_content.char_indices().for_each(|(i, c)| {
+            row.column_offsets.push(row.render.content.len());
+            let height = traced.heights[i + row.offset];
+            let value = (height - traced.min_height + 1) as f64 / (traced.max_height - traced.min_height + 1) as f64;
+            let hash = traced.stack_hashes[i + row.offset];
+            queue!(
+                row.render,
+                // ResetColor
+                SetBackgroundColor(Color::Rgb {
+                    r: (value * 255.0) as u8,
+                    g: (hash & 0xff) as u8,
+                    b: ((hash >> 8) & 0xff) as u8,
+                 })
+            ).unwrap();
             index += 1;
             if c == '\t' {
                 row.render.push(' ');
@@ -155,6 +250,7 @@ impl EditorRows {
                 row.render.push(c);
             }
         });
+        row.column_offsets.push(row.render.content.len());
     }
 }
 
@@ -259,11 +355,11 @@ impl CursorController {
     }
 }
 
-struct EditorContents {
+struct RenderBuf {
     content: String,
 }
 
-impl EditorContents {
+impl RenderBuf {
     fn new() -> Self {
         Self {
             content: String::new(),
@@ -277,9 +373,16 @@ impl EditorContents {
     fn push_str(&mut self, string: &str) {
         self.content.push_str(string)
     }
+
+    fn flush_to_screen(&mut self) -> io::Result<()> {
+        let out = write!(stdout(), "{}", self.content);
+        stdout().flush()?;
+        self.content.clear();
+        out
+    }
 }
 
-impl io::Write for EditorContents {
+impl io::Write for RenderBuf {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         match std::str::from_utf8(buf) {
             Ok(s) => {
@@ -291,16 +394,13 @@ impl io::Write for EditorContents {
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        let out = write!(stdout(), "{}", self.content);
-        stdout().flush()?;
-        self.content.clear();
-        out
+        Ok(())
     }
 }
 
 struct Output {
     win_size: (usize, usize),
-    editor_contents: EditorContents,
+    editor_contents: RenderBuf,
     cursor_controller: CursorController,
     editor_rows: EditorRows,
     status_message: StatusMessage,
@@ -313,7 +413,7 @@ impl Output {
             .unwrap();
         Self {
             win_size,
-            editor_contents: EditorContents::new(),
+            editor_contents: RenderBuf::new(),
             cursor_controller: CursorController::new(win_size),
             editor_rows: EditorRows::from_file(path),
             status_message: StatusMessage::new(
@@ -398,14 +498,16 @@ impl Output {
             } else {
                 let row = self.editor_rows.get_editor_row(file_row);
                 let render = &row.render;
+                let column_offsets = &row.column_offsets;
                 let column_offset = self.cursor_controller.column_offset;
-                let len = cmp::min(render.len().saturating_sub(column_offset), screen_columns);
+                let len = cmp::min(column_offsets.len().saturating_sub(column_offset + 1), screen_columns);
                 let start = if len == 0 { 0 } else { column_offset };
-                let render = render.chars().skip(start).take(len).collect::<String>();
+                let render = render.content[column_offsets[start] .. column_offsets[start + len]].to_string();
                 self.editor_contents.push_str(&render);
             }
             queue!(
                 self.editor_contents,
+                ResetColor,
                 terminal::Clear(ClearType::UntilNewLine)
             )
             .unwrap();
@@ -431,7 +533,7 @@ impl Output {
             cursor::MoveTo(cursor_x as u16, cursor_y as u16),
             cursor::Show
         )?;
-        self.editor_contents.flush()
+        self.editor_contents.flush_to_screen()
     }
 }
 
@@ -544,13 +646,31 @@ impl Editor {
     }
 }
 
+fn with_screen<T>(f: impl FnOnce() -> T) -> T {
+    terminal::enable_raw_mode().unwrap();
+
+    let hook = std::panic::take_hook();
+    // TODO: unset this hook when we leave, or use catch_unwind
+    std::panic::set_hook(Box::new(move |e| {
+        terminal::disable_raw_mode().expect("Unable to disable raw mode");
+        Output::clear_screen().expect("error");
+        // println!("panicking!");
+        std::io::stdout().flush().unwrap();
+        hook(e);
+    }));
+
+    let res = f();
+
+    res
+}
+
 fn main() -> crossterm::Result<()> {
-    let _clean_up = CleanUp;
     let mut args = std::env::args().skip(1).collect::<Vec<String>>();
     assert_eq!(args.len(), 1);
     let path = args.pop().unwrap().into();
     let mut editor = Editor::from_file(path);
-    terminal::enable_raw_mode()?;
-    while editor.run()? {}
-    Ok(())
+    with_screen(|| {
+        while editor.run()? {}
+        Ok(())
+    })
 }
